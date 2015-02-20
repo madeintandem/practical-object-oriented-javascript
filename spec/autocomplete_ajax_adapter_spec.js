6c75ea1c70f52f749304b0d2edadaa32170169a7
Dayton Nolan <daytonn@gmail.com>
2015-02-19 22:46:13 -0600

    AutocompleteAjaxAdapter tests

---
+++ spec/autocomplete_ajax_adapter_spec.js
@@ -0,0 +1,142 @@
+describe("AutocompleteAjaxAdapter", function() {
+  var subject;
+  var onAutocompleteSpy;
+  var xhr;
+  var requests;
+  beforeEach(function() {
+    xhr = sinon.useFakeXMLHttpRequest();
+    requests = [];
+    xhr.onCreate = function(xhr) {
+      requests.push(xhr);
+    };
+    onAutocompleteSpy = sinon.spy();
+    subject = new AutocompleteAjaxAdapter({
+      url: "test",
+      onAutocomplete: onAutocompleteSpy
+    });
+  });
+
+  xit("requires a url", function() {
+    expect(function() {
+      new AutocompleteAjaxAdapter;
+    }).to.throw("AutocompleteAjaxAdapter: url is undefined");
+  });
+
+  xit("has a url", function() {
+    expect(subject.url).to.equal("test");
+  });
+
+  xit("has an onAutocomplete handler", function() {
+    expect(subject.onAutocomplete).to.equal(onAutocompleteSpy);
+  });
+
+  xit("has a throttle delay", function() {
+    expect(subject.throttleDelay).to.be.a("number");
+  });
+
+  describe("defaults", function() {
+    beforeEach(function() {
+      subject = new AutocompleteAjaxAdapter({
+        url: "test"
+      });
+    });
+
+    xit("has a default onAutocomplete handler", function() {
+      expect(subject.onAutocomplete).to.be.a("function");
+    });
+  });
+
+  describe("#handleTextEntry", function() {
+    var text;
+    beforeEach(function() {
+      text = "test query";
+      sinon.spy(window, "clearTimeout");
+      sinon.spy(subject, "queueRequest");
+    });
+
+    afterEach(function() {
+      clearTimeout.restore();
+    });
+
+    xit("sets the query", function() {
+      subject.handleTextEntry(text);
+      expect(subject.query).to.equal(text);
+    });
+
+    xit("queues a request", function() {
+      subject.handleTextEntry(text);
+      expect(subject.queueRequest).to.have.been.called;
+    });
+
+    describe("when there's a queued request", function() {
+      xit("clears the timeout", function() {
+        subject.queuedRequest = 5;
+        subject.handleTextEntry(text);
+        expect(clearTimeout).to.have.been.calledWith(5);
+      });
+    });
+
+    describe("when there is not a queued request", function() {
+      xit("does nothing", function() {
+        subject.handleTextEntry(text);
+        expect(clearTimeout).not.to.have.been.called;
+      });
+    });
+
+    describe("when the text is empty", function() {
+      xit("does not queue a request", function() {
+        subject.handleTextEntry("");
+        expect(subject.queuedRequest).to.be.undefined;
+      });
+
+      xit("clears a request out", function() {
+        subject.queuedRequest = 15;
+        subject.handleTextEntry("");
+        expect(clearTimeout).to.have.been.calledWith(subject.queuedRequest);
+      });
+
+      xit("sends an empty array to the on autocomplete callback", function() {
+        subject.handleTextEntry("");
+        expect(subject.onAutocomplete).to.have.been.calledWith([]);
+      });
+    });
+  });
+
+  describe("#fetchItems", function() {
+    xit("gets the items via ajax", function() {
+      subject.fetchItems();
+      var request = _.last(requests);
+      expect(request.url).to.equal(subject.url);
+      expect(request.method).to.equal("GET");
+      expect(request.requestHeaders.Accept).to.match(/application\/json/);
+    });
+  });
+
+  describe("#queueRequest", function() {
+    beforeEach(function() {
+      subject.throttleDelay = 0;
+      sinon.spy(window, "setTimeout");
+      sinon.spy(subject, "fetchItems");
+      subject.queueRequest();
+    });
+
+    afterEach(function() {
+      setTimeout.restore();
+    });
+
+    xit("queues a fetch items request", function() {
+      expect(subject.queuedRequest).to.be.a("number");
+    });
+
+    xit("fetches the items after the throttle delay", function() {
+      expect(setTimeout).to.have.been.called;
+
+      var args = _.first(setTimeout.args);
+      var callback = _.first(args);
+      var throttleDelay = _.last(args);
+      callback();
+      expect(subject.fetchItems).to.have.been.called;
+      expect(throttleDelay).to.equal(subject.throttleDelay);
+    });
+  });
+});
